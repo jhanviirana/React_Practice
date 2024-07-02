4ccf344dc6024821d401dabc6c5efb4b4005d120
jhanviirana <jhanvi.rana8903@gmail.com>
2024-07-02 15:15:58 +0530

    customReact

---
+++ customReact/customreact.js
@@ -0,0 +1,38 @@
+// import { Children } from "react"
+
+function customRender(reactElement,container){
+    // const domElement = document.createElement(reactElement.type)
+
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href',reactElement.props.href)
+    // domElement.setAttribute('target',reactElement.props.target)
+
+    // container.appendChild(domElement)
+
+    const domElement = document.createElement(reactElement.type)
+
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) 
+    {
+        // if(prop === 'children') continue;
+
+        domElement.setAttribute(prop, reactElement.props[prop])    
+        
+    }
+    container.appendChild(domElement)
+
+}
+
+const mainContanier = document.querySelector('#root')
+
+const reactElement = {
+    type : 'a',
+    props : {
+        href:'https://google.com',
+        target : '_blank'
+    },
+    children : 'Click on me to visit google',
+
+}
+
+customRender(reactElement,mainContanier);
